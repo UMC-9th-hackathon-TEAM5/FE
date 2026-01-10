f313b1f4a76d943dbf77ba99233a7ea617981a7b
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 15:42:47 +0900

    setting:대략적인 Layout 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import MobileLayout from "./layouts/MobileLayout";
 
 function App() {
   return (
-    <main className="flex h-screen max-w-full items-center justify-center bg-black">
-      <div className="h-screen w-107.5 border border-white">안녕</div>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MobileLayout />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/layouts/MobileLayout.tsx
@@ -0,0 +1,14 @@
+import { Outlet } from "react-router-dom";
+
+export default function MobileLayout() {
+  return (
+    <main className="flex h-screen w-full items-center justify-center bg-[#1f1f1f]">
+      <div className="flex h-203 w-93.75 flex-col border border-white px-4 py-6">
+        <header className="flex h-8 w-full items-center justify-center bg-white">
+          있을지도 모르는 Header 영역
+        </header>
+        <Outlet />
+      </div>
+    </main>
+  );
+}
