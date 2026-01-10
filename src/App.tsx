518682352272405acfa6da2b124120b9bd734de1
goody <ehdgusqaz98@gmail.com>
2026-01-10 18:32:48 +0900

    feat : RoleButton 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,10 @@ import "./App.css";
 import "./App.css";
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MobileLayout from "@/layouts/MobileLayout";
 
-// 테스트 페이지에 대한 라우팅
-import HeaderTestPage from "@/pages/test/HeaderTestPage";
-import InputTestPage from "@/pages/test/InputTestPage";
-import PartyInfoCardTestPage from "./pages/test/PartyInfoCardTestPage";
-import PlayerBadgeTestPage from "./pages/test/PlayerBadgeTestPage";
+import MobileLayout from "./layouts/MobileLayout";
+import HeaderTestPage from "./pages/test/HeaderTestPage";
 import RoleButtonTestPage from "./pages/test/RoleButtonTestPage";
-import ButtonTestPage from "./pages/test/ButtonTestPage";
-
-// 실제 페이지
-import PartyDetailPage from "./pages/party/PartyDetailPage";
-import CreatePartyPage from "./pages/party/CreatePartyPage";
 
 function App() {
   return (
@@ -25,23 +16,7 @@ function App() {
           {/* 테스트 페이지에 대한 라우팅 */}
           <Route path="/" element={<MobileLayout />}>
             <Route path="test/header" element={<HeaderTestPage />} />
-            <Route path="test/input" element={<InputTestPage />} />
-            <Route
-              path="test/party-info-card"
-              element={<PartyInfoCardTestPage />}
-            />
-            <Route
-              path="/test/player-badge"
-              element={<PlayerBadgeTestPage />}
-            />
-
-            {/* 실제 페이지 */}
-            <Route path="party/detail" element={<PartyDetailPage />} />
-            <Route path="party/create" element={<CreatePartyPage />} />
-
-            <Route path="test/Button" element={<ButtonTestPage />} />
             <Route path="test/RoleButton" element={<RoleButtonTestPage />} />
-            <Route path="test/Button" element={<ButtonTestPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- src/components/common/RoleButton.tsx
+++ src/components/common/RoleButton.tsx
@@ -1,6 +1,5 @@
 import { cva, type VariantProps } from "class-variance-authority";
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
 
 const buttonStyles = cva(
   clsx(
@@ -48,23 +47,21 @@ interface RoleButtonProps
 }
 
 export const RoleButton = ({
+  className,
   state,
   roleType,
-  className,
   ...props
 }: RoleButtonProps) => {
   const content = ROLE_DATA[roleType];
+
   return (
     <button
       type="button"
-      className={twMerge(buttonStyles({ state, roleType }), className)}
+      className={buttonStyles({ state, roleType })}
       {...props}
     >
       <div className="flex w-full items-center justify-center gap-2 text-sm">
         <p className="">{content.icon}</p>
-        <p className="leading-[140%] font-normal tracking-[-0.4px] whitespace-nowrap">
-          {content.label}
-        </p>
         <p className="leading-[140%] font-normal tracking-[-0.4px] whitespace-nowrap">
           {content.label}
         </p>
