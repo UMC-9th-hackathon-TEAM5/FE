7fa16a49b8cb8cd751acd55a95bb07ba21c8b8ff
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-11 04:32:54 +0900

    feat:PlayerResultPage 구현

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,8 @@ import WaitingPartyPage from "./pages/party/WaitingPartyPage";
 import ModalTestPage from "./pages/test/ModalTestPage";
 import GameStartPage from "./pages/game/GameStartPage";
 import GamePlayPage from "./pages/game/GamePlayPage";
+import GameResultPage from "./pages/game/GameResultPage";
+
 import HomePage from "./pages/HomePage";
 
 function App() {
@@ -62,6 +64,7 @@ function App() {
             <Route path="party/waiting" element={<WaitingPartyPage />} />
             <Route path="game/start" element={<GameStartPage />} />
             <Route path="game/playing" element={<GamePlayPage />} />
+            <Route path="game/result" element={<GameResultPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
