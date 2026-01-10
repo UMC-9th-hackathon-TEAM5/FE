88ae8756362937ce8b24d4bc5af7e9f52507e67a
goody <ehdgusqaz98@gmail.com>
2026-01-10 22:18:18 +0900

    feat : button들 구현

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,22 +9,32 @@ const buttonStyles = cva(
     "h-9.5 py-2",
     "rounded-lg",
     "flex items-center justify-center",
-    "text-white text-medium leading-[140%] tracking-[-0.4px] font-medium",
+    "text-white leading-[140%] tracking-[-0.4px] font-medium",
   ),
   {
     variants: {
       width: {
         default: "w-18",
         xl: "w-[310px]",
+        lg : "w-[300px]",
         md: "w-[149px]",
+        base: "w-[72px]",
         sm: "w-[60px]",
       },
+      // state == style?
       state: {
         // default : inactive
         default: "border border-main-dark1",
         active: "bg-main-dark1",
+        arrive: "bg-[#10B981]",
+        non_arrive : "bg-[#E3E6EA]",
+        escape : "bg-[#EF4444]",
+        arrest : "bg-black border border-[#808080]",
+        prison : "bg-[#EF444433] border border-[#EF4444]",
+        instagram : "rounded-none border-2 border-transparent [background:linear-gradient(#1f1f1f,#1f1f1f)_padding-box,linear-gradient(to_right,#833AB4,#EE2A7B,#F9CE34)_border-box] h-11",
       },
     },
+<<<<<<< HEAD
     compoundVariants: [
       // confirm button
       {
@@ -41,6 +51,14 @@ const buttonStyles = cva(
         width: "sm",
         className: "bg-[#E3E6EA] text-[#808080] text-sm",
       },
+=======
+    compoundVariants : [
+      // Confirm Button
+      {state : "active", width: "xl", className: "bg-main rounded-none shadow-[2px_2px_0_0_#008E58] h-11 font-bold text-black text-[16px]"},
+      {state : "default", width: "xl", className: "h-11 rounded-none"},
+      // GameEnd Button
+      {state : "active", width: "lg", className: "bg-main rounded-none shadow-[2px_2px_0_0_#008E58] h-11 font-bold text-black text-[16px]"},
+>>>>>>> 9fcebd0 (feat : button들 구현)
     ],
     defaultVariants: {
       state: "default",
@@ -52,11 +70,21 @@ const buttonStyles = cva(
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonStyles>;
 
+<<<<<<< HEAD
 export const Button = ({ width, children, state, ...props }: ButtonProps) => {
+=======
+export const Button = ({
+  width,
+  children,
+  state,
+  className,
+  ...props
+}: ButtonProps) => {
+>>>>>>> 9fcebd0 (feat : button들 구현)
   return (
     <button
       type="button"
-      className={twMerge(buttonStyles({ width, state }))}
+      className={twMerge(buttonStyles({ width, state }), className)}
       {...props}
     >
       {children}
--- src/pages/test/ButtonTestPage.tsx
+++ src/pages/test/ButtonTestPage.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/common/Button";
+<<<<<<< HEAD
 import CheckIcon from "@/assets/check/check.svg?react";
 
 export default function ButtonTestPage() {
@@ -7,12 +8,23 @@ export default function ButtonTestPage() {
     // 2. overflow-y-auto: 내용이 길어지면 이 페이지 안에서 스크롤 생김
     // 3. no-scrollbar: 스크롤바 숨김 (선택 사항, index.css 설정 필요)
     <div className="h-full w-full overflow-y-auto bg-[#111] p-4 text-white">
+=======
+import CheckIcon from "@/assets/check/check.svg?react"
+import LockIcon from "@/assets/lock/lock.svg?react"
+import ShareIcon from "@/assets/share/share.svg?react"
+
+export default function ButtonTestPage() {
+  return (
+    <div className="h-full w-full overflow-y-auto bg-[#111] text-white p-4">
+      
+>>>>>>> 9fcebd0 (feat : button들 구현)
       {/* 타이틀 영역 */}
       <div className="mb-8 text-center">
         <h1 className="text-main-dark1 text-xl font-bold">Button System</h1>
         <p className="text-xs text-gray-500">Scroll to see all variants</p>
       </div>
       <div className="flex flex-col gap-10 pb-10">
+<<<<<<< HEAD
         {/* 1. Small Buttons */}
         <section className="flex flex-col gap-3">
           <h2 className="border-b border-gray-700 pb-2 text-sm font-bold text-gray-400">
@@ -86,6 +98,85 @@ export default function ButtonTestPage() {
             <Button width="xl" disabled>
               비활성화 버튼
             </Button>
+=======
+
+        {/* Default Button */}
+        <section className="flex flex-col gap-3">
+            <h2 className="text-sm font-bold border-b  border-gray-700 pb-2 text-gray-400">
+              Default
+            </h2>
+            <Button state="active" >Button</Button>
+            <Button state="default">Button</Button>
+          </section>  
+
+        {/* 도착/미도착 */}
+        <section className="flex flex-col gap-3">
+          <h2 className="text-sm font-bold border-b  border-gray-700 pb-2 text-gray-400">
+            도착 / 미도착
+          </h2>
+          <Button width="sm" state="arrive" className="text-[14px] pl-2.5 pr-1.5">
+            <span className="flex whitespace-nowrap">도착 <CheckIcon /> </span>
+          </Button>
+          <Button width="sm" state="non_arrive" className="text-[#808080] text-[14px]">미도착</Button>
+        </section>
+
+        {/* Confirm Button */}
+        <section className="flex flex-col gap-3">
+          <h2 className="text-sm font-bold border-b border-gray-700 pb-2 text-gray-400">
+            Confirm Button
+          </h2>
+          <div className="flex flex-col items-center gap-3 bg-gray-900/50 p-4 rounded-lg">
+            <Button width="xl" state="default">취소하기</Button>
+            <Button width="xl" state="active">확인 완료</Button>
+          </div>
+        </section>
+
+        {/* Role State Button */}
+        <section className="flex flex-col gap-3">
+          <h2 className="text-sm font-bold border-b border-gray-700 pb-2 text-gray-400">
+            Role State Buttons
+          </h2>
+          <div className="flex flex-col items-center gap-3 bg-gray-900/50 p-4 rounded-lg">
+            <Button width="base" state="escape" className="text-[14px]">
+              탈출 성공
+            </Button>
+            <Button width="base" state="prison">
+              <div className="flex gap-2 items-center justify-center text-[14px] tracking-[-0.35px]" >
+                <LockIcon/>감옥
+              </div>
+            </Button>
+            <Button width="base" state="arrest">
+              <div className="flex gap-2 items-center justify-center text-[14px] tracking-[-0.35px]">
+                <LockIcon/>검거
+              </div>
+            </Button>
+          </div>
+        </section>
+
+        {/* 모달 버튼*/}
+        <section className="flex flex-col gap-3">
+          <h2 className="text-sm font-bold border-b border-gray-700 pb-2 text-gray-400">
+            Modal Button
+          </h2>
+          <div className="flex flex-col items-center gap-3 bg-gray-900/50 p-4 rounded-lg">
+            <Button width="lg" state="active">게임 종료</Button>
+            <Button width="lg" state="active">확인</Button>
+          </div>
+        </section>
+        
+        {/* 게임 결과 페이지 */}
+        <section className="flex flex-col gap-3">
+          <h2 className="text-sm font-bold border-b border-gray-700 pb-2 text-gray-400">
+            게임종료
+          </h2>
+          <div className="flex flex-col items-center gap-3 bg-gray-900/50 p-4 rounded-lg">
+            <Button width="xl" state="instagram">
+              <div className="flex gap-2 items-center">
+                <ShareIcon /> Instargram 스토리로 공유하기
+              </div>
+            </Button>
+            <Button width="xl" state="active">메인으로 돌아가기</Button>
+>>>>>>> 9fcebd0 (feat : button들 구현)
           </div>
         </section>
       </div>
