9dc43ebf69d84bb6b26f519ea5c508c0bc6420b2
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 23:59:59 +0900

    fix:에러 및 경로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,15 @@ import MobileLayout from "@/layouts/MobileLayout";
 // 테스트 페이지에 대한 라우팅
 import HeaderTestPage from "@/pages/test/HeaderTestPage";
 import InputTestPage from "@/pages/test/InputTestPage";
-import PartyInfoCardTestPage from "./pages/test/PartyInfoCardTestPage";
-import PlayerBadgeTestPage from "./pages/test/PlayerBadgeTestPage";
-import RoleButtonTestPage from "./pages/test/RoleButtonTestPage";
-import ButtonTestPage from "./pages/test/ButtonTestPage";
+import PartyInfoCardTestPage from "@/pages/test/PartyInfoCardTestPage";
+import RoleButtonTestPage from "@/pages/test/RoleButtonTestPage";
+import ButtonTestPage from "@/pages/test/ButtonTestPage";
+import PlayerBadgeTestPage from "@/pages/test/PlayerBadgeTestPage";
+import CreatePartyPage from "@/pages/party/CreatePartyPage";
 
 // 실제 페이지
-import PartyDetailPage from "./pages/party/PartyDetailPage";
-import CreatePartyPage from "./pages/party/CreatePartyPage";
+import PartyDetailPage from "@/pages/party/PartyDetailPage";
+import LoginPage from "@/pages/LoginPage";
 
 function App() {
   return (
