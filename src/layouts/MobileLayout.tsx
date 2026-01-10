38377287f3f6a8238f1d4a40f5442fc08764f03b
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 17:47:29 +0900

    setting:svgr 라이브러리 사용을 위한 세팅

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -5,7 +5,7 @@ export default function MobileLayout() {
   return (
     <main className="flex min-h-screen w-full justify-center bg-[#1f1f1f] pt-10">
       <div className="flex h-211 w-97.5 flex-col border border-white px-4">
-        <Header title="안녕" />
+        <Header title="팟 상세" />
         <Outlet />
       </div>
     </main>
