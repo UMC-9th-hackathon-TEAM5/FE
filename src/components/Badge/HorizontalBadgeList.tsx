10b00a0d6befa460f7074bbdd96febaac5b72260
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 20:52:26 +0900

    feat:HorizontalPlayBadgeList 컴포넌트 구현

---
+++ src/components/Badge/HorizontalBadgeList.tsx
@@ -0,0 +1,25 @@
+import clsx from "clsx";
+import PlayerBadge from "./playerBadge";
+
+interface HorizontalBadgeListProps {
+  items: string[];
+  className?: string;
+}
+
+export default function HorizontalBadgeList({
+  items,
+  className = "",
+}: HorizontalBadgeListProps) {
+  return (
+    <div
+      className={clsx(
+        "scrollbar-hide flex items-center gap-3 overflow-x-auto py-2",
+        className,
+      )}
+    >
+      {items.map((item, index) => (
+        <PlayerBadge key={`${item}-${index}`} player={item} />
+      ))}
+    </div>
+  );
+}
