4e2353327bc06a29c17c4758ba951536fd9db702
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 21:28:13 +0900

    wip-page:"팟 상세" 페이지 구현 중

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -7,14 +7,7 @@ export default function MobileLayout() {
       <div className="relative flex h-211 w-97.5 flex-col border border-white">
 =======
     <main className="flex min-h-screen w-full justify-center bg-[#1f1f1f] pt-10">
-<<<<<<< HEAD
-      <div className="flex h-211 w-97.5 flex-col border border-white px-4 py-6">
-        <div className="flex h-8 w-full items-center justify-center bg-white">
-          있을지도 모르는 Header 영역
-        </div>
->>>>>>> 6acac36 (setting:MoblieLayout width 및 height 수정)
-=======
-      <div className="flex h-211 w-97.5 flex-col border border-white px-4">
+      <div className="flex h-211 w-97.5 flex-col border border-white">
         <Outlet />
       </div>
     </main>
