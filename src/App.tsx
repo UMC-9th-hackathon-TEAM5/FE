0307e36b7947e03a061ce588f89a03dfcca192f1
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 22:12:20 +0900

    feat-wip-page:팟 만들기 페이지 일부 구현

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MobileLayout from "./layouts/MobileLayout";
 import HeaderTestPage from "./pages/test/HeaderTestPage";
 import RoleButtonTestPage from "./pages/test/RoleButtonTestPage";
-import ButtonTestPage from "./pages/test/ButtonTestPage";
+
+// 실제 페이지
+import PartyDetailPage from "./pages/party/PartyDetailPage";
+import CreatePartyPage from "./pages/party/CreatePartyPage";
 
 function App() {
   return (
@@ -15,8 +18,20 @@ function App() {
           {/* 테스트 페이지에 대한 라우팅 */}
           <Route path="/" element={<MobileLayout />}>
             <Route path="test/header" element={<HeaderTestPage />} />
-            <Route path="test/RoleButton" element={<RoleButtonTestPage />} />
-            <Route path="test/Button" element={<ButtonTestPage />} />
+            <Route path="test/input" element={<InputTestPage />} />
+            <Route
+              path="test/party-info-card"
+              element={<PartyInfoCardTestPage />}
+            />
+            <Route
+              path="/test/player-badge"
+              element={<PlayerBadgeTestPage />}
+            />
+            <Route path="/test/roll-button" element={<RoleButtonTestPage />} />
+
+            {/* 실제 페이지 */}
+            <Route path="party/detail" element={<PartyDetailPage />} />
+            <Route path="party/create" element={<CreatePartyPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ src/pages/party/CreatePartyPage.tsx
@@ -0,0 +1,75 @@
+import Header from "@/components/common/Header";
+import InputLabel from "@/components/common/Input/InputLabel";
+import Input from "@/components/common/Input/Input";
+
+export default function CreatePartyPage() {
+  return (
+    <>
+      <Header title="팟 만들기" />
+      <main className="h-full w-full border border-white px-10">
+        <section
+          className="flex flex-col border border-white py-3"
+          role="파티 제목 입력"
+        >
+          <InputLabel label="팟 제목" isRequired={true} />
+          <Input placeholder="예 : 수지구 경도팟 모임" required />
+        </section>
+
+        <section
+          className="flex flex-col border border-white py-3"
+          role="파티 모임 일정 정하기"
+        >
+          <InputLabel label="모임 일시" isRequired={true} />
+          <Input type="datetime-local" required />
+        </section>
+
+        <section
+          className="flex flex-col border border-white pt-3"
+          role="파티 장소 정하기"
+        >
+          <InputLabel label="모임 장소" isRequired={true} />
+          <Input placeholder="예 : 수지생태공원 분수대 앞" required />
+        </section>
+
+        <section className="flex flex-col border border-white py-3">
+          <InputLabel label="모집 인원" />
+          <div className="flex justify-center gap-2">
+            <div className="flex flex-col">
+              <InputLabel label="경찰" className="mb-1" />
+              <Input placeholder={"0"} width="sm" type="number" min={0} />
+            </div>
+
+            <div className="flex flex-col">
+              <InputLabel label="도둑" className="mb-1" />
+              <Input placeholder={"0"} width="sm" type="number" min={0} />
+            </div>
+          </div>
+        </section>
+
+        <section
+          className="flex flex-col border border-white pt-3"
+          role="게임 시간 정하기"
+        >
+          <InputLabel label="시간 설정" className="mb-4" />
+
+          <InputLabel label="게임 진행 시간 (분)" className="mb-1" />
+          <Input defaultValue={60} type="number" className="mb-4" />
+          <InputLabel label="도망 갈 시간 (초)" className="mb-1" />
+          <Input defaultValue={60} type="number" />
+          <span className="text-main-variant mt-1 text-xs">
+            게임 시작 직후 도둑들이 숨을 시간입니다
+          </span>
+        </section>
+
+        <section
+          className="flex flex-col border border-white pt-3"
+          role="게임 시간 정하기"
+        >
+          <InputLabel label="설명" className="mb-4" />
+          <Input placeholder="경도팟에 대한 설명을 적어주세요" />
+        </section>
+      </main>
+      ;
+    </>
+  );
+}
