2aee42d4bb6e126f7f511d82a902579bef007f38
goody <ehdgusqaz98@gmail.com>
2026-01-10 18:32:48 +0900

    feat : RoleButton 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MobileLayout from "@/layouts/MobileLayout";
 
+<<<<<<< HEAD
 // 테스트 페이지에 대한 라우팅
 import HeaderTestPage from "@/pages/test/HeaderTestPage";
 import InputTestPage from "@/pages/test/InputTestPage";
@@ -14,7 +15,11 @@ import ButtonTestPage from "./pages/test/ButtonTestPage";
 // 실제 페이지
 import PartyDetailPage from "./pages/party/PartyDetailPage";
 import CreatePartyPage from "./pages/party/CreatePartyPage";
-import CreatePartyPage from "./pages/party/CreatePartyPage";
+=======
+import MobileLayout from "./layouts/MobileLayout";
+import HeaderTestPage from "./pages/test/HeaderTestPage";
+import RoleButtonTestPage from "./pages/test/RoleButtonTestPage";
+>>>>>>> dfa6f9e (feat : RoleButton 컴포넌트 구현)
 
 function App() {
   return (
@@ -24,6 +29,7 @@ function App() {
           {/* 테스트 페이지에 대한 라우팅 */}
           <Route path="/" element={<MobileLayout />}>
             <Route path="test/header" element={<HeaderTestPage />} />
+<<<<<<< HEAD
             <Route path="test/input" element={<InputTestPage />} />
             <Route
               path="test/party-info-card"
@@ -37,6 +43,9 @@ function App() {
             {/* 실제 페이지 */}
             <Route path="party/detail" element={<PartyDetailPage />} />
             <Route path="party/create" element={<CreatePartyPage />} />
+=======
+            <Route path="test/RoleButton" element={<RoleButtonTestPage />} />
+>>>>>>> dfa6f9e (feat : RoleButton 컴포넌트 구현)
           </Route>
         </Routes>
       </BrowserRouter>
