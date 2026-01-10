4400f85e1b1cc9b821c9a11b45de9a3cbc769835
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 14:27:04 +0900

    setting:mobile size에 맞춰 width 세팅:

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -2,7 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <body className="h-screen max-w-full bg-black text-white">안녕하세요.</body>
+    <main className="flex h-screen max-w-full items-center justify-center bg-black">
+      <div className="h-screen w-107.5 border border-white">안녕</div>
+    </main>
   );
 }
 
