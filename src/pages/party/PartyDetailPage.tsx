208c89045580c7b6b0852d5550dbf0900eed4350
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 21:05:39 +0900

    feat-page:"팟 상세" 페이지 구현 시작

---
+++ src/pages/party/PartyDetailPage.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/common/Header";
+
+export default function PartyDetailPage() {
+  return (
+    <>
+      <Header title="팟 상세" />
+      <main className=""></main>;
+    </>
+  );
+}
