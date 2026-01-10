323fafa7d6029e6e3f02291b3f9181a045eb85bb
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 23:44:57 +0900

    design:ArrivalStatusButton 수정

--- src/components/common/Card/PlayerArrivalCard/ArrivalStatusButton.tsx
+++ src/components/common/Card/PlayerArrivalCard/ArrivalStatusButton.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { twMerge } from "tailwind-merge";
+import CheckIcon from "@/assets/check/check.svg?react";
 
 interface ArrivalStatusButtonProps {
   isArrived: boolean;
@@ -13,14 +14,15 @@ export const ArrivalStatusButton: React.FC<ArrivalStatusButtonProps> = ({
       type="button"
       disabled
       className={twMerge(
-        "rounded px-3 py-1 text-sm font-semibold transition-colors select-none",
+        "flex h-9 w-15 items-center justify-center rounded-lg text-sm font-medium transition-colors select-none",
         isArrived
-          ? "cursor-default bg-green-500 text-white"
-          : "cursor-default bg-gray-300 text-gray-700",
+          ? "bg-[#10B981] pl-2 text-white"
+          : "bg-[#E3E6EA] text-gray-700",
       )}
       aria-label={isArrived ? "Arrived" : "Not arrived"}
     >
       {isArrived ? "도착" : "미도착"}
+      {isArrived && <CheckIcon className="text-[#E3E6EA]" />}
     </button>
   );
 };
