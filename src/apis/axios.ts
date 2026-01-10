163f18400173081200ef343a7fc6076302707100
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-11 05:56:47 +0900

    fix:로그인 관련 cors 해결

--- src/apis/axios.ts
+++ src/apis/axios.ts
@@ -5,7 +5,7 @@ export const axiosInstance = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
-  withCredentials: true,
+  withCredentials: false,
 });
 
 axiosInstance.interceptors.request.use((config) => {
