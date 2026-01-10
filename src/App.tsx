c6b472c069f0b9c286320e80658e9c73c7238d3e
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 20:47:46 +0900

    feat:playBadge 컴포넌트 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,28 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MobileLayout from "@/layouts/MobileLayout";
 
+// 테스트 페이지에 대한 라우팅
 import HeaderTestPage from "@/pages/test/HeaderTestPage";
 import InputTestPage from "@/pages/test/InputTestPage";
 import PartyInfoCardTestPage from "./pages/test/PartyInfoCardTestPage";
+import PlayerBadgeTestPage from "./pages/test/PlayerBadgeTestPage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          {/* 테스트 페이지에 대한 라우팅 */}
           <Route path="/" element={<MobileLayout />}>
             <Route path="test/header" element={<HeaderTestPage />} />
             <Route path="test/input" element={<InputTestPage />} />
             <Route
               path="test/party-info-card"
               element={<PartyInfoCardTestPage />}
+            />
+            <Route
+              path="/test/player-badge"
+              element={<PlayerBadgeTestPage />}
             />
           </Route>
         </Routes>
