7c2a00dcfd4f060c015d2fde1eaf0661a0c8bf6f
goody <ehdgusqaz98@gmail.com>
2026-01-11 00:30:54 +0900

    fix : 모달 팝업 위치 absoulte로 수정

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -2,8 +2,8 @@ import { Outlet } from "react-router-dom";
 
 export default function MobileLayout() {
   return (
-    <main className="flex min-h-screen w-full justify-center bg-black pt-10">
-      <div className="flex h-211 w-97.5 flex-col border border-white">
+    <main className="flex min-h-screen w-full justify-center bg-[#111111] pt-10">
+      <div className="relative flex h-211 w-97.5 flex-col border border-white">
         <Outlet />
       </div>
     </main>
