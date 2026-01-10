85db224e5ce98872893142f44e417afe8a2164ec
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 21:05:39 +0900

    feat-page:"팟 상세" 페이지 구현 시작

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import InputTestPage from "@/pages/test/InputTestPage";
 import PartyInfoCardTestPage from "./pages/test/PartyInfoCardTestPage";
 import PlayerBadgeTestPage from "./pages/test/PlayerBadgeTestPage";
 
+// 실제 페이지
+import PartyDetailPage from "./pages/party/PartyDetailPage";
+
 function App() {
   return (
     <>
@@ -26,6 +29,9 @@ function App() {
               path="/test/player-badge"
               element={<PlayerBadgeTestPage />}
             />
+
+            {/* 실제 페이지 */}
+            <Route path="party/detail" element={<PartyDetailPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
