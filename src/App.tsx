40a129ae5061dda03abb154d66d38e8522181943
goody <ehdgusqaz98@gmail.com>
2026-01-10 23:43:45 +0900

    feat : BaseModal 구현

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,25 @@ import HeaderTestPage from "@/pages/test/HeaderTestPage";
 import InputTestPage from "@/pages/test/InputTestPage";
 import PartyInfoCardTestPage from "./pages/test/PartyInfoCardTestPage";
 import PlayerBadgeTestPage from "./pages/test/PlayerBadgeTestPage";
+<<<<<<< HEAD
 import PlayerArrivalCardTestPage from "./pages/test/PlayerArrivalCardTestPage";
+=======
+
+>>>>>>> b389011 (feat : BaseModal 구현)
 
 // 실제 페이지
 import PartyDetailPage from "./pages/party/PartyDetailPage";
 import LoginPage from "./pages/LoginPage";
 <<<<<<< HEAD
+<<<<<<< HEAD
 import CreatePartyPage from "./pages/party/CreatePartyPage";
 import WaitingPartyPage from "./pages/party/WaitingPartyPage";
 =======
 >>>>>>> 583fb23 (feat : 로그인 페이지 구현)
+=======
+import CreatePartyPage from "./pages/party/CreatePartyPage";
+import ModalTestPage from "./pages/test/ModalTestPage";
+>>>>>>> b389011 (feat : BaseModal 구현)
 
 function App() {
   return (
@@ -37,13 +46,20 @@ function App() {
               path="test/player-arrival-card"
               element={<PlayerArrivalCardTestPage />}
             />
+<<<<<<< HEAD
             <Route path="test/Button" element={<ButtonTestPage />} />
+=======
+            <Route path="test/modal" element={<ModalTestPage />} />
+>>>>>>> b389011 (feat : BaseModal 구현)
 
             {/* 실제 페이지 */}
             <Route path="login" element={<LoginPage />} />
             <Route path="party/detail" element={<PartyDetailPage />} />
             <Route path="party/create" element={<CreatePartyPage />} />
+<<<<<<< HEAD
             <Route path="party/waiting" element={<WaitingPartyPage />} />
+=======
+>>>>>>> b389011 (feat : BaseModal 구현)
           </Route>
         </Routes>
       </BrowserRouter>
