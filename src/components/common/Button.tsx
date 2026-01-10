d8a656b9934e50756964a0c62fbd62ed9a32c53c
goody <ehdgusqaz98@gmail.com>
2026-01-10 22:18:18 +0900

    feat : button들 구현

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -9,7 +9,7 @@ const buttonStyles = cva(
     "h-9.5 py-2",
     "rounded-lg",
     "flex items-center justify-center",
-    "text-white text-medium leading-[140%] tracking-[-0.4px] font-medium",
+    "text-white leading-[140%] tracking-[-0.4px] font-medium",
   ),
   {
     variants: {
@@ -17,30 +17,40 @@ const buttonStyles = cva(
         default: "w-18",
         default: "w-18",
         xl: "w-[310px]",
+        lg: "w-[300px]",
         md: "w-[149px]",
+        base: "w-[72px]",
         sm: "w-[60px]",
       },
+      // state == style?
       state: {
         // default : inactive
         default: "border border-main-dark1",
         active: "bg-main-dark1",
+        arrive: "bg-[#10B981]",
+        non_arrive: "bg-[#E3E6EA]",
+        escape: "bg-[#EF4444]",
+        arrest: "bg-black border border-[#808080]",
+        prison: "bg-[#EF444433] border border-[#EF4444]",
+        instagram:
+          "rounded-none border-2 border-transparent [background:linear-gradient(#1f1f1f,#1f1f1f)_padding-box,linear-gradient(to_right,#833AB4,#EE2A7B,#F9CE34)_border-box] h-11",
       },
     },
     compoundVariants: [
-      // confirm button
+      // Confirm Button
       {
         state: "active",
         width: "xl",
         className:
-          "text-black bg-main rounded-none shadow-[2px_2px_0_0_#008E58] h-11 font-bold",
+          "bg-main rounded-none shadow-[2px_2px_0_0_#008E58] h-11 font-bold text-black text-[16px]",
       },
       { state: "default", width: "xl", className: "h-11 rounded-none" },
-      // 참여자 목록 - 도착/미도착
-      { state: "active", width: "sm", className: "bg-[#10B981] text-sm" },
+      // GameEnd Button
       {
-        state: "default",
-        width: "sm",
-        className: "bg-[#E3E6EA] text-[#808080] text-sm",
+        state: "active",
+        width: "lg",
+        className:
+          "bg-main rounded-none shadow-[2px_2px_0_0_#008E58] h-11 font-bold text-black text-[16px]",
       },
     ],
     defaultVariants: {
@@ -53,11 +63,17 @@ const buttonStyles = cva(
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
   VariantProps<typeof buttonStyles>;
 
-export const Button = ({ width, children, state, ...props }: ButtonProps) => {
+export const Button = ({
+  width,
+  children,
+  state,
+  className,
+  ...props
+}: ButtonProps) => {
   return (
     <button
       type="button"
-      className={twMerge(buttonStyles({ width, state }))}
+      className={twMerge(buttonStyles({ width, state }), className)}
       {...props}
     >
       {children}
