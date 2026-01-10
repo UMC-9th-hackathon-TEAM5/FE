d0328761ddb6f2dc7de9296f56a4d15d9c0dc98d
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-11 04:42:44 +0900

    fix:type 에러 수정

--- src/pages/game/GameStartPage.tsx
+++ src/pages/game/GameStartPage.tsx
@@ -14,7 +14,10 @@ const GameStartPage = () => {
   const navigate = useNavigate();
 
   const isHost = true; // 호스트 여부
-  const [role, setRole] = useState<"police" | "thief">("thief");
+  type Role = "police" | "thief";
+
+  // TODO: 이후 서버 응답 또는 라우트 state로 교체 예정
+  const role = "thief" as Role;
 
   const [gameStatus, setGameStatus] = useState<"idle" | "ready" | "action">(
     "idle",
