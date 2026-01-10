576da8fd8b7a19337a084e97cfd410c3b06c8d7a
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 23:15:41 +0900

    design:팟 상세 페이지 구현 완료

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function MobileLayout() {
   return (
-    <main className="flex min-h-screen w-full justify-center bg-[#1f1f1f] pt-10">
+    <main className="flex min-h-screen w-full justify-center bg-black pt-10">
       <div className="flex h-211 w-97.5 flex-col border border-white">
         <Outlet />
       </div>
