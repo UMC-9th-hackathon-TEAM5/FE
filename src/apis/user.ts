3d2b11c9989fcea929c87f64163b26b08bb0830b
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-11 05:42:13 +0900

    fix/apis 수정

--- src/apis/user.ts
+++ src/apis/user.ts
@@ -1,19 +1,33 @@
 import { axiosInstance } from "./axios";
 
+export type ApiResponse<T> = {
+  timestamp: string;
+  status: number;
+  code: string;
+  message: string;
+  path: string;
+  data: T;
+};
+
 type RequestUserDto = {
   nickname: string;
   password: string;
   lat: number;
   lng: number;
 };
-type ResponseUserDto = {
-  timestamp: string;
-  status: number;
+type ResponseUserData = {
+  userId: number;
+  nickname: string;
+  accessToken: string;
+  tokenType: string;
 };
 
 export const postUser = async (
   body: RequestUserDto,
-): Promise<ResponseUserDto> => {
-  const { data } = await axiosInstance.post("/api/v1/user/session", body);
+): Promise<ApiResponse<ResponseUserData>> => {
+  const { data } = await axiosInstance.post<ApiResponse<ResponseUserData>>(
+    "/api/v1/user/session",
+    body,
+  );
   return data;
 };
