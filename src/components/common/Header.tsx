a00445213e977cc68108d08a27881fec053e9fb4
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 17:34:23 +0900

    feat:Header 컴포넌트 임시 구현

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,13 +1,12 @@
 import { cva, VariantProps } from "class-variance-authority";
 import clsx from "clsx";
 
-import HeaderArrowIcon from "@/assets/arrow/arrow_back.svg?react";
-
 const headerStyles = cva(
   clsx(
     "w-full h-15",
-    "flex items-center px-5 py-2.5",
+    "flex items-center px-2 py-2.5",
     "text-2xl font-bold text-main",
+    "border border-white",
   ),
 );
 
@@ -15,11 +14,12 @@ type HeaderStyleProps = VariantProps<typeof headerStyles>;
 
 type HeaderProps = {
   title?: React.ReactNode;
+  leftChild?: React.ReactNode;
   className?: string;
   onLeftClick?: () => void;
 } & HeaderStyleProps;
 
-const Header = ({ title, onLeftClick, className }: HeaderProps) => {
+const Header = ({ title, leftChild, onLeftClick, className }: HeaderProps) => {
   return (
     <header className={clsx(headerStyles(), "justify-between", className)}>
       <button
@@ -27,12 +27,11 @@ const Header = ({ title, onLeftClick, className }: HeaderProps) => {
         onClick={onLeftClick}
         className="flex shrink-0 items-center justify-start gap-2"
       >
-        <HeaderArrowIcon />
+        {leftChild}
       </button>
       {title && (
         <div className="mx-2 line-clamp-2 flex-1 text-center">{title}</div>
       )}
-      <div className="h-6 w-6"></div>
     </header>
   );
 };
--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -1,12 +1,11 @@
+import Header from "@/components/common/Header";
 import { Outlet } from "react-router-dom";
 
 export default function MobileLayout() {
   return (
     <main className="flex min-h-screen w-full justify-center bg-[#1f1f1f] pt-10">
-      <div className="flex h-211 w-97.5 flex-col border border-white px-4 py-6">
-        <div className="flex h-8 w-full items-center justify-center bg-white">
-          있을지도 모르는 Header 영역
-        </div>
+      <div className="flex h-211 w-97.5 flex-col border border-white px-4">
+        <Header title="안녕" />
         <Outlet />
       </div>
     </main>
