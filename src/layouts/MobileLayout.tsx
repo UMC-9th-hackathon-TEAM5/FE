25bb096f79caf2000f09a28c1ce342bf9a2ebd99
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-10 17:21:03 +0900

    setting:MoblieLayout width 및 height 수정

--- src/layouts/MobileLayout.tsx
+++ src/layouts/MobileLayout.tsx
@@ -2,8 +2,16 @@ import { Outlet } from "react-router-dom";
 
 export default function MobileLayout() {
   return (
+<<<<<<< HEAD
     <main className="flex min-h-screen w-full justify-center bg-[#111111] pt-10">
       <div className="relative flex h-211 w-97.5 flex-col border border-white">
+=======
+    <main className="flex min-h-screen w-full justify-center bg-[#1f1f1f] pt-10">
+      <div className="flex h-211 w-97.5 flex-col border border-white px-4 py-6">
+        <div className="flex h-8 w-full items-center justify-center bg-white">
+          있을지도 모르는 Header 영역
+        </div>
+>>>>>>> 6acac36 (setting:MoblieLayout width 및 height 수정)
         <Outlet />
       </div>
     </main>
