c1bfd42d9b2b28ff1dc2a62bbf8805405c167d64
theSnackOverflow <ivory.ma9ic@gmail.com>
2026-01-11 01:26:32 +0900

    fix:불필요한 경로 제거

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import MobileLayout from "@/layouts/MobileLayout";
 // 테스트 페이지에 대한 라우팅
 import HeaderTestPage from "@/pages/test/HeaderTestPage";
 import InputTestPage from "@/pages/test/InputTestPage";
+<<<<<<< HEAD
 import PartyInfoCardTestPage from "./pages/test/PartyInfoCardTestPage";
 import PlayerBadgeTestPage from "./pages/test/PlayerBadgeTestPage";
 import PlayerArrivalCardTestPage from "./pages/test/PlayerArrivalCardTestPage";
@@ -15,6 +16,17 @@ import PartyDetailPage from "./pages/party/PartyDetailPage";
 import LoginPage from "./pages/LoginPage";
 import CreatePartyPage from "./pages/party/CreatePartyPage";
 import WaitingPartyPage from "./pages/party/WaitingPartyPage";
+=======
+import PartyInfoCardTestPage from "@/pages/test/PartyInfoCardTestPage";
+import RoleButtonTestPage from "@/pages/test/RoleButtonTestPage";
+import ButtonTestPage from "@/pages/test/ButtonTestPage";
+import PlayerBadgeTestPage from "@/pages/test/PlayerBadgeTestPage";
+import CreatePartyPage from "@/pages/party/CreatePartyPage";
+
+// 실제 페이지
+import PartyDetailPage from "@/pages/party/PartyDetailPage";
+import LoginPage from "@/pages/LoginPage";
+>>>>>>> 68d9a07 (fix:에러 및 경로 변경)
 
 function App() {
   return (
